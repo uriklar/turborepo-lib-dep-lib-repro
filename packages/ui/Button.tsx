83a49f2eb8c6c41e7f70d31f66f1e4f67d1ffb7b
Uri Klar <uriklar@gmail.com>
2022-01-31 11:39:36 +0200

    repro

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { Button as AnotherButton} from "anotherui"
 export const Button = () => {
-  return <button>Boop</button>;
+  return <><button>Boop</button><AnotherButton/></>;
 };
